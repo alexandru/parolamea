aecadcc268d97975dedfa8606fc1d40ef6bbdb91
Alexandru Nedelcu <alexandru@nedelcu.net>
2014-12-30 02:50:29 +0200

    Add license

--- src/main/assets/libs/bootstrap-3.3.1.js
+++ src/main/assets/libs/bootstrap-3.3.1.js
@@ -1,7 +1,18 @@
-/*!
- * Bootstrap v3.3.1 (http://getbootstrap.com)
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+/*
+ * Copyright (C) 2014 Alexandru Nedelcu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 if (typeof jQuery === 'undefined') {
